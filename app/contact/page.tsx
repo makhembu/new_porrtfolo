b8e300faf1cdd0a137c16e65b0d8c1d9337bad73
Brian Makhembu <31996990+makhembu@users.noreply.github.com>
2023-12-29 02:00:35 -0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/makhembu",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@makhembu",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:makhembu.brian@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "makhembu.brian@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/makhembu",
 		label: "Github",
-		handle: "chronark",
+		handle: "makhembu",
 	},
 ];
 
